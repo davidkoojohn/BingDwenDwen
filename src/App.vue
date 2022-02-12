bb9d00e25a1fbabb526ab27b397bd0b7eb63702e
guozhao <davidkoojohn@163.com>
2022-02-12 16:59:38 +0800

    update models path

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,10 @@ import treeTexture from './assets/images/tree.png';
 import flagTexture from './assets/images/flag.png';
 import "./libs/GLTFLoader.js"
 
-const landModel = './models/land.glb';
-const treeModel = './models/tree.gltf';
-const bingdundunModel = './models/bingdwendwen.glb';
-const flagModel = './models/flag.glb';
+const landModel = '../models/land.glb';
+const treeModel = '../models/tree.gltf';
+const bingdundunModel = '../models/bingdwendwen.glb';
+const flagModel = '../models/flag.glb';
 
 
 const loadingProcess = ref<number>(0)
